fec13d1c67788d346d0c32cef066bdd427139eda
atherfawaz <atherfawaz@users.noreply.github.com>
2020-04-06 15:22:40 +0500

    Completed file reading

--- gd.py
+++ gd.py
@@ -1,9 +1,22 @@
-#GRADIENT DESCENT ON IRIS/FISCHER DATASET
-
+# GRADIENT DESCENT ON IRIS/FISCHER DATASET
 import pandas as pd
 import numpy as np
 
-dataset = pd.read_excel('dataset.xlsx')
-sepal_width = dataset['Sepal width']
-sepal_length = dataset['Sepal length']
-petal_length = dataset['Petal length']
+# MAIN FUNCTION
+if __name__ == "__main__":
+
+    dataset = pd.read_excel('dataset.xlsx')
+    sepal_width = dataset['Sepal width']
+    sepal_length = dataset['Sepal length']
+    petal_length = dataset['Petal length']
+    flower_class = dataset['Species']
+
+    idx = 0
+    for x in flower_class:
+        if (x == 'Setosa'):
+            flower_class[idx] = 0
+        elif (x == 'Versicolor'):
+            flower_class[idx] = 1
+        elif (x == 'Virginica'):
+            flower_class[idx] = 2
+        idx += 1
